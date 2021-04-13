4797fedb6058acb9ed84c77f8aaa8e6145c6c767
@sam.rubarema <sam.rubarema@bposeats.com>
2021-04-13 17:27:02 +0200

    Updates

---
+++ precache-manifest.583a324e4a16c32037e0edc1463705d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a138314b2a5e7c8684c8",
+    "url": "/MyData/static/css/main.921a8968.chunk.css"
+  },
+  {
+    "revision": "a138314b2a5e7c8684c8",
+    "url": "/MyData/static/js/main.a138314b.chunk.js"
+  },
+  {
+    "revision": "ba0ab9d3dd4527964f43",
+    "url": "/MyData/static/js/runtime~main.ba0ab9d3.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/MyData/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2701f36a9893626da2f579f72d47ec6c",
+    "url": "/MyData/index.html"
+  }
+];
